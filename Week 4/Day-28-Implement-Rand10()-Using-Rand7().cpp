8bb083c5e9dd2c0cfe368be66e705062e3a961e7
Vikas Sangwan <vikassangwan003@gmail.com>
2020-08-29 08:01:21 +0530

    Medium math random

---
+++ Week 4/Day-28-Implement-Rand10()-Using-Rand7().cpp
@@ -0,0 +1,43 @@
+/**
+Problem : https://leetcode.com/explore/challenge/card/august-leetcoding-challenge/552/week-4-august-22nd-august-28th/3439/
+
+Given the API rand7 which generates a uniform random integer in the range 1 to 7, write a function rand10 which generates a uniform random integer in the range 1 to 10. You can only call the API rand7 and you shouldn't call any other API. Please don't use the system's Math.random().
+
+Notice that Each test case has one argument n, the number of times that your implemented function rand10 will be called while testing. 
+
+Follow up:
+
+What is the expected value for the number of calls to rand7() function?
+Could you minimize the number of calls to rand7()?
+ 
+
+Example 1:
+
+Input: n = 1
+Output: [2]
+Example 2:
+
+Input: n = 2
+Output: [2,8]
+Example 3:
+
+Input: n = 3
+Output: [3,8,10]
+
+**/
+
+//Solution
+// The rand7() API is already defined for you.
+// int rand7();
+// @return a random integer in the range 1 to 7
+
+class Solution {
+public:
+    int rand10() {
+        int rand40 = 40;
+        while (rand40 >= 40) {
+            rand40 = (rand7() - 1) * 7 + rand7() - 1;
+        }
+        return rand40 % 10 + 1;
+    }
+};
